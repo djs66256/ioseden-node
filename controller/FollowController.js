ac11b0d9046733504ab12b4602ddf33a7dff6a00
Daniel <djs66256@163.com>
2016-05-19 03:11:52 +0800

    feature: follow somebody

--- controller/FollowController.js
+++ controller/FollowController.js
@@ -4,11 +4,46 @@
 'use strict';
 import Follower from '../model/Follower';
 import Followee from '../model/Followee';
+import User from '../model/User';
 
 let Controller = {
 
     follow({from, to}) {
-        
+        function validate() {
+            return new Promise((resolve, reject) => {
+                if (from == to) {
+                    resolve();
+                }
+                else {
+                    reject(new Error('自己不能关注自己'));
+                }
+            });
+        }
+        function createFollower() {
+            return new Promise((resolve, reject) => {
+                User.findOne({where: {id: to}}).then(user => {
+                    if (user) {
+                        user.addFollower([from]).then(resolve).catch(reject);
+                    }
+                    else {
+                        throw new Error('用户不存在');
+                    }
+                }).catch(reject);
+            });
+        }
+        function createFollowee() {
+            return new Promise((resolve, reject) => {
+                User.findOne({where: {id: from}}).then(user => {
+                    if (user) {
+                        user.addFollowee([to]).then(resolve).catch(reject);
+                    }
+                    else {
+                        throw new Error('用户不存在');
+                    }
+                }).catch(reject);
+            });
+        }
+        return Promise.all([validate(), createFollower(), createFollowee()]);
     },
 
     deleteFollow({from, to}) {
--- routes/follow.js
+++ routes/follow.js
@@ -6,6 +6,76 @@
 
 import express from 'express';
 let router = express.Router();
+import needLogin from '../libs/needLogin';
+import {Return, Success, Fail} from '../libs/return';
 import FollowController from '../controller/FollowController';
+
+router.post('/user/followee/:id', needLogin(), (req, res) => {
+    let ownerId = req.userId;
+    let followeeId = req.params.id;
+    FollowController.follow({from: ownerId, to: followeeId}).then(() => {
+        res.send(Success());
+    }).catch(err => {
+        res.send(Fail(err.message));
+    });
+});
+
+router.delete('/user/followee/:id', needLogin(), (req, res) => {
+    let ownerId = req.userId;
+    let followeeId = req.params.id;
+    FollowController.deleteFollow({from: ownerId, to: followeeId}).then(() => {
+        res.send(Success());
+    }).catch(err => {
+        res.send(Fail(err.message));
+    });
+});
+
+router.get('/user/:id/followers', (req, res) => {
+    FollowController.findFollowers({
+        userId: req.params.id,
+        limit: req.body.limit,
+        offset: req.body.offset
+    }).then(followers => {
+        res.send(Success(followers));
+    }).catch(err => {
+        res.send(Fail(err.message));
+    });
+});
+
+router.get('/user/followers', needLogin(), (req, res) => {
+    FollowController.findFollowers({
+        userId: req.userId,
+        limit: req.body.limit,
+        offset: req.body.offset
+    }).then(followers => {
+        res.send(Success(followers));
+    }).catch(err => {
+        res.send(Fail(err.message));
+    });
+});
+
+router.get('/user/:id/followees', (req, res) => {
+    FollowController.findFollowees({
+        userId: req.params.id,
+        limit: req.body.limit,
+        offset: req.body.offset
+    }).then(followees => {
+        res.send(Success(followees));
+    }).catch(err => {
+        res.send(Fail(err.message));
+    });
+});
+
+router.get('/user/followees', needLogin(), (req, res) => {
+    FollowController.findFollowees({
+        userId: req.userId,
+        limit: req.body.limit,
+        offset: req.body.offset
+    }).then(followees => {
+        res.send(Success(followees));
+    }).catch(err => {
+        res.send(Fail(err.message));
+    });
+});
 
 module.exports = router;
